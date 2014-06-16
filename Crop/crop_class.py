4fa4d9202aa0390b2a09d28634b54aba059ad418
Oliver <oliveranderson-chapman@gmail.com>
2014-06-16 16:02:54 +0100

    Crop Tasks and introduction to object Oriented Pr

---
+++ Crop/crop_class.py
@@ -0,0 +1,31 @@
+class Crop:
+    """ A generic food crop"""
+
+    #constructor
+    def __init__(self,growth_rate, light_need, water_need):
+        #set the attributes with an initial value
+        self._growth = 0
+        self._days_growing = 0
+        self._growth_rate = growth_rate
+        self._light_need = light_need
+        self._water_need = water_need
+        self._status = "Seed"
+        self._type = "Generic"
+
+        #the above attributes are prefixed with an underscore to indicate
+        #that they should not be accesses directly from outwith the class
+
+def main(): # initiate the class
+    new_crop = Crop(1,4,3)
+    #test to see if it works
+    print(new_crop._status)
+    print(new_crop._light_need)
+    print(new_crop._water_need)
+    new_crop2 = Crop(2,5,7)
+    print(new_crop2._status)
+    print(new_crop2._light_need)
+    print(new_crop2._water_need)
+
+if __name__ == "__main__":
+    main()
+        
